068252f35f9137796069e77ce8c3e6f55a2e1afe
Fazalcode1 <126341873+Fazalcode1@users.noreply.github.com>
2023-07-05 06:21:49 +0530

    Update CVRP.cpp

--- CVRP.cpp
+++ CVRP.cpp
@@ -34,16 +34,23 @@ vector<vector<vector<double>>> randomise(vector<vector<vector<double>>> z){
      return z;
 }
 
-vector<int> dfs_visit(int node,int visited[], vector<vector<vector<double>>> z, vector<int> p){
-      visited[node]=1;
-      p.push_back(node);
-      for(auto x:z[node]){
-          if(visited[(int)x[0]]==-1){
-            p=dfs_visit(x[0],visited,z,p);
-          }
-      }
-     return p;
+vector<int> dfs_visit(int node, int visited[], const vector<vector<vector<double>>>& z) {
+    visited[node] = 1;
+    vector<int> permutation;
+    permutation.push_back(node);
+    int index = 0;
+    while (index < permutation.size()) {
+        int current = permutation[index++];
+        for (auto x : z[current]) {
+            if (visited[(int)x[0]] == -1) {
+                visited[(int)x[0]] = 1;
+                permutation.push_back((int)x[0]);
+            }
+        }
+    }
+    return permutation;
 }
+
 
 vector<vector<int>> convert_to_roots(int Depot,vector<int> p, double Demand[],double Capacity){
     vector<vector<int>> Routes;
@@ -208,15 +215,14 @@ int main(){
     }
 
     z = prims_mst(V,z);
-    
     double minCost = INT_MAX;
     vector<vector<int>> minRoutes;
-    for(int i=1;i<=pow(10,5);i++){
+    for(int i=1;i<=pow(10,4);i++){
         vector<int> permutation;
         int visited[V];
         memset(visited,-1,sizeof(visited));
         z = randomise(z);
-        permutation = dfs_visit(depot,visited,z,permutation);
+        permutation = dfs_visit(depot,visited,z);
         vector<vector<int>> Routes = convert_to_roots(depot,permutation,Demand,Capacity);
         double Cost = cost(Routes,depot,points);
         if(Cost<minCost){
@@ -227,10 +233,11 @@ int main(){
     minRoutes = Refine_Routes(minRoutes,V,points);
     minCost = cost(minRoutes,depot,points);
     cout<<"minimum cost = "<<minCost<<endl;
-    // cout<<"minimum cost routes from depot:"<<endl;
+    cout<<"minimum cost routes from depot:"<<endl;
     // for(auto x:minRoutes){
     //     for(auto y:x){
     //         cout<<y<<" ";
+    //         break;
     //     }
     //     cout<<endl;
     // }
